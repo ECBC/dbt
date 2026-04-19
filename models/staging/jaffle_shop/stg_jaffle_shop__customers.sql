b9bb46bd9c73aff91003ceeb8303f8619d27148b
ECBC <enrique.cortinabermudezdecastro@edhec.com>
2026-04-19 15:40:54 +0000

    corrected stg jaffle shop customers

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,20 +1,6 @@
-with 
-
-source as (
-
-    select * from {{ source('jaffle_shop', 'customers') }}
-
-),
-
-renamed as (
-
     select
-        id,
+        id as customer_id,
         first_name,
         last_name
 
-    from source
-
-)
-
-select * from renamed
+    from {{ source('jaffle_shop', 'customers') }}
